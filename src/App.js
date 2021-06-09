3365c74a14f87fa6e628c6801f65417b51d56dae
Jalexander01 <60295314+Jalexander01@users.noreply.github.com>
2021-06-09 16:37:23 -0500

    updates

--- src/App.js
+++ src/App.js
@@ -14,8 +14,3 @@ function App() {
 
 export default App;
 
-
-//6. Make sure that the final website is styled like the example shown here:
-//https://l1pp6.csb.app/
-
-//HINT: You will need to study the classes in teh styles.css file to appy styling.
